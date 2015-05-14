d153099f0e812ee33557ab0f2694338bb8b9ecfa
Silver Lao <silver.lao@gmail.com>
2015-05-14 10:55:43 +0800

    一些bug

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 var payment = require('./lib/payment');
 
-//exports.sendRedpack = redpack.sendRedpack;
-//exports.Redpack = redpack.Redpack;
-
-exports.sendPayment = redpack.sendPayment;
-exports.Payment = redpack.Payment;
+exports.sendPayment = payment.sendPayment;
+exports.Payment = payment.Payment;
